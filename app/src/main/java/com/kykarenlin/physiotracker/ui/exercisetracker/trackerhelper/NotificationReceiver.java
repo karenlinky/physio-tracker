fe503607d9ff414abd34dabfcb556df0ec8fe74e
Karen Lin <karenlinky@gmail.com>
2023-11-25 19:51:02 -0500

    openning app when notification is clicked

--- app/src/main/java/com/kykarenlin/physiotracker/ui/exercisetracker/trackerhelper/NotificationReceiver.java
+++ app/src/main/java/com/kykarenlin/physiotracker/ui/exercisetracker/trackerhelper/NotificationReceiver.java
@@ -4,6 +4,7 @@ import static com.kykarenlin.physiotracker.ui.exercisetracker.trackerhelper.Stop
 
 import android.Manifest;
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.kykarenlin.physiotracker.MainActivity;
 import com.kykarenlin.physiotracker.R;
 
 public class NotificationReceiver extends BroadcastReceiver {
@@ -25,6 +27,11 @@ public class NotificationReceiver extends BroadcastReceiver {
         String title = intent.getStringExtra(StopwatchNotificationObserver.INTENT_KEY_TITLE);
         String text = intent.getStringExtra(StopwatchNotificationObserver.INTENT_KEY_TEXT);
 
+        Intent openAppIntent = new Intent(context, MainActivity.class);
+        openAppIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingOpenAppIntent = PendingIntent.getActivity(context, 10, openAppIntent, PendingIntent.FLAG_IMMUTABLE);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, STOPWATCH_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_run_circle)
                 .setContentTitle(title)
@@ -32,7 +39,8 @@ public class NotificationReceiver extends BroadcastReceiver {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setVibrate(new long[] {200, 200, 500});
+                .setVibrate(new long[] {200, 200, 500})
+                .setContentIntent(pendingOpenAppIntent);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(1, builder.build());
