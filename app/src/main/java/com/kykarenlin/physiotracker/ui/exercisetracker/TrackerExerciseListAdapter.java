a480f549911853af79a7727c5c93a0170e6d0803
Karen Lin <karenlinky@gmail.com>
2023-11-27 22:47:09 -0500

    Supporting dark mode for tracker list

--- app/src/main/java/com/kykarenlin/physiotracker/ui/exercisetracker/TrackerExerciseListAdapter.java
+++ app/src/main/java/com/kykarenlin/physiotracker/ui/exercisetracker/TrackerExerciseListAdapter.java
@@ -1,6 +1,9 @@
 package com.kykarenlin.physiotracker.ui.exercisetracker;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kykarenlin.physiotracker.R;
@@ -21,33 +25,49 @@ import java.util.List;
 public class TrackerExerciseListAdapter extends RecyclerView.Adapter<TrackerExerciseListAdapter.TrackerExerciseListHolder> {
     private List<ExerciseProgress> exercisesWithProgress = new ArrayList<>();
     private OnItemClickListener listener;
+
+    private Context context;
     @NonNull
     @Override
     public TrackerExerciseListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
+        this.context = parent.getContext();
+        View itemView = LayoutInflater.from(context)
                 .inflate(R.layout.tracker_exercise_item, parent, false);
         return new TrackerExerciseListHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TrackerExerciseListHolder holder, int position) {
+        int nightModeFlags = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        boolean isNightMode = nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+
+        int enabledColor = ContextCompat.getColor(context, R.color.trackerListEnabled);
+        int selectedColor = ContextCompat.getColor(context, R.color.trackerListSelected);
+        int disabledColor = ContextCompat.getColor(context, R.color.trackerListDisabled);
+
+        if (isNightMode) {
+            enabledColor =  ContextCompat.getColor(context, R.color.trackerListEnabledDark);
+            selectedColor =  ContextCompat.getColor(context, R.color.trackerListSelectedDark);
+            disabledColor =  ContextCompat.getColor(context, R.color.trackerListDisabledDark);
+        }
+
         ExerciseProgress currentExercise = exercisesWithProgress.get(position);
         holder.txtTrackerExerciseItemName.setText(currentExercise.getExercise().getName());
         if (currentExercise.getExercise().getSessionStatus().equals(ExerciseSessionStatus.COMPLETED.toString())) {
             // exercise completed
-            holder.trackerItemCard.setBackgroundColor(Color.parseColor("#FFEDEDED"));
+            holder.trackerItemCard.setBackgroundColor(disabledColor);
             holder.trackerItemIconStatusContainer.setVisibility(View.VISIBLE);
         } else if (currentExercise.getButtonDisabled()) {
             // session completed or session in progress
-            holder.trackerItemCard.setBackgroundColor(Color.parseColor("#FFEDEDED"));
+            holder.trackerItemCard.setBackgroundColor(disabledColor);
             holder.trackerItemIconStatusContainer.setVisibility(View.INVISIBLE);
         } else if (currentExercise.getSelected()) {
             // exercise gets selected
-            holder.trackerItemCard.setBackgroundColor(Color.parseColor("#FFebf4ff"));
+            holder.trackerItemCard.setBackgroundColor(selectedColor);
             holder.trackerItemIconStatusContainer.setVisibility(View.INVISIBLE);
         } else {
             // exercise not completed
-            holder.trackerItemCard.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+            holder.trackerItemCard.setBackgroundColor(enabledColor);
             holder.trackerItemIconStatusContainer.setVisibility(View.INVISIBLE);
         }
     }
