3db63bb9ea253aa24e488d78803dd892eb3284ae
Karen Lin <karenlinky@gmail.com>
2023-12-04 20:52:26 -0500

    Fixed filter keyword not showing up bug

--- app/src/main/java/com/kykarenlin/physiotracker/ui/eventlogs/filters/FilterKeyword.java
+++ app/src/main/java/com/kykarenlin/physiotracker/ui/eventlogs/filters/FilterKeyword.java
@@ -46,6 +46,6 @@ public class FilterKeyword extends EventFilter{
 
     @Override
     public void updateUIElement() {
-        this.getUIElement().setText(this.getIsActivated() ? "" : FilterKeyword.keyword);
+        this.getUIElement().setText(this.getIsActivated() ? FilterKeyword.keyword : "");
     }
 }
